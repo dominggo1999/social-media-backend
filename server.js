b4fac2366e8fd3d653f07a5deebe3982948adebd
dominggo1999 <arnolddominggo19@gmail.com>
2021-09-14 00:31:47 +0700

    Set Up Dependencies

--- server.js
+++ server.js
@@ -1,16 +1,29 @@
-const express = require("express");
-const morgan = require("morgan");
+require('dotenv').config();
+
+const express = require('express');
+const morgan = require('morgan');
+const fs = require('fs');
+const path = require('path');
+const cors = require('cors');
+
+// Import routers here
+const authRouter = require('./routes/auth');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Morgan logger
-app.use(morgan('tiny'));
+// app.use(morgan('tiny'));
 
+// Middleware
+app.use(cors());
+app.use(express.json()); // based on body parser
+app.use(express.urlencoded());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// Use routers here
+// Example             app.use('/api/users/', userRouter);
+app.use('/api/auth/', authRouter);
 
 app.listen(PORT, () => {
-  console.log(`Example app listening at http://localhost:${PORT}`)
-})
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});
